39db5562a09ee62df799b0fdcde455b54763b694
root <root@localhost.localdomain>
2016-01-13 20:08:52 -0700

    NK_Scan Version 2.0

--- nk_scan.py
+++ nk_scan.py
@@ -1,5 +1,6 @@
-#IT WORKS!!!!!!!!!!!!!!!!!!!!!!
-import nmap
+from libnmap.process import NmapProcess
+from libnmap.parser import NmapParser, NmapParserException
+
 
 print """
 
@@ -62,25 +63,69 @@ print """
 
 
 
+def do_scan(targets, options):
+    parsed = None
+    nmproc = NmapProcess(targets, options)
+    rc = nmproc.run()
+    if rc != 0:
+        print("nmap scan failed: {0}".format(nmproc.stderr))
+    print(type(nmproc.stdout))
 
-nm = nmap.PortScanner()
+    try:
+        parsed = NmapParser.parse(nmproc.stdout)
+    except NmapParserException as e:
+        print("Exception raised while parsing scan: {0}".format(e.msg))
 
-nm.scan(hosts='175.45.176.0/22 210.52.109.0/24 77.94.35.0/24', arguments='-n -sT -A -p 1-65535 -vvv -T4 -oN output.txt')
-#insert NK ranges into hosts
+    return parsed
 
-for host in nm.all_hosts():
-  print('----------------------------------------------------')
-  print('Host : %s (%s)' % (host, nm[host].hostname()))
-  print('State : %s' % nm[host].state())
 
-for proto in nm[host].all_protocols():
-  print('----------')
-  print('Protocol : %s' % proto)
+def print_scan(nmap_report):
+    print("Starting Nmap {0} ( http://nmap.org ) at {1}".format(
+        nmap_report.version,
+        nmap_report.started))
 
-  lport = nm[host][proto].keys()
-  lport.sort()
-  for port in lport:
-    print('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+    for host in nmap_report.hosts:
+        if len(host.hostnames):
+            tmp_host = host.hostnames.pop()
+        else:
+            tmp_host = host.address
+
+        print("Nmap scan report for {0} ({1})".format(
+            tmp_host,
+            host.address))
+        print("Host is {0}.".format(host.status))
+        print("  PORT     STATE         SERVICE")
+
+        for serv in host.services:
+            pserv = "{0:>5s}/{1:3s}  {2:12s}  {3}".format(
+                    str(serv.port),
+                    serv.protocol,
+                    serv.state,
+                    serv.service)
+            if len(serv.banner):
+                pserv += " ({0})".format(serv.banner)
+            print(pserv)
+    print(nmap_report.summary)
+
+
+if __name__ == "__main__":
+    report = do_scan("175.45.176.0/22", "-n -sT -A -p 1-65535 -vvv -T5")
+    if report:
+        print_scan(report)
+    else:
+        print("No results returned")
+
+    report = do_scan("210.52.109.0/24", "-n -sT -A -p 1-65535 -vvv -T5")
+    if report:
+        print_scan(report)
+    else:
+        print("No results returned")
+
+    report = do_scan("77.94.35.0/24", "-n -sT -A -p 1-65535 -vvv -T5")
+    if report:
+        print_scan(report)
+    else:
+        print("No results returned")
 
 print('----------------------------------------------------')
 print ""
